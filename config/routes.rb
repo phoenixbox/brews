d23a2ff4c00d5f40f2391261edd12477347ba09f
Shane Rogers <rogerssh@tcd.ie>
2013-06-14 18:40:20 -0600

    user creation and validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Brews::Application.routes.draw do
   get "sessions/new"
 
-  get "users/new"
   resources :games
+  resources :users
   resources :sessions, :only => [:create]
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
