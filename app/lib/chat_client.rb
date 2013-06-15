450818d5255404f58268c8308e3b501a77847a74
Christopher <ChrisKnight.mail@gmail.com>
2013-06-15 17:31:29 -0600

    GET and POST api methods talkin to the fay app

--- app/lib/chat_client.rb
+++ app/lib/chat_client.rb
@@ -3,16 +3,18 @@ attr_reader :game_id
 
   def initialize(game_id)
     @game_id = game_id
+    #Pass in url ? good idea
   end
 
   def get_messages
-    json = RestClient.get( message_url )
-    JSON.parse( json )
+    # json = RestClient.get( message_url )
+    # JSON.parse( json )
+    RestClient.get( message_url )
   end
 
-  def post_message
-    json = something.to_json
-    RestClient.post( post_message_url, :game_id => 1 )
+  def create_message(params)
+    # json = something.to_json
+    RestClient.post( post_message_url, params.to_json, :content_type => :json )
   end
 
   # RestClient.post 'http://example.com/resource', :param1 => 'one', :nested => { :param2 => 'two' }
