36e9a4c6dc5830d6411f622cc2e8e4a528d8c271
Shane Rogers <rogerssh@tcd.ie>
2013-06-16 08:49:16 -0600

    homepage styling-hero unit and captioned thumbnails

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,8 +19,8 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
-    chat_client = ChatClient.new(@game.id)
-    @messages = chat_client.get_messages
+    # chat_client = ChatClient.new(@game.id)
+    # @messages = chat_client.get_messages
     # @message = chat_client.post_message
   end
 end
