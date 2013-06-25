0bca3dce75c0c29e95098dabe5d5d9c5ad0d3b5d
Shane Rogers <rogerssh@tcd.ie>
2013-06-25 14:11:29 -0600

    vcr testing setup - added c.allow_http_connections_when_no_cassette = true

---
+++ spec/controllers/messages_create.rb
@@ -0,0 +1,18 @@
+describe "MessagesController#create" do
+
+  let(:message_data){{team_name: "hi", content: "Howdy", game_id: 1}}
+
+  before(:all) do
+    @chat = ChatClient.new(1)
+    @chat.stub(:create_message).with(message_data)
+  end
+
+  it "sends messages to the async pubsub server" do
+    # Channel.should_receive(:publish).with(message_data)
+    # post messages_path(message_data)
+    expect {
+      post :create, message_data
+    } 
+    # post messages_path(message_data)
+  end
+end
--- spec/features/authenticated_users/creates_game_spec.rb
+++ spec/features/authenticated_users/creates_game_spec.rb
@@ -4,17 +4,20 @@ describe "authenticated user starts and stops a new game" do
 
   let(:user){FactoryGirl.create(:user)}
 
-  it "logs in and creates a game" do
-    login(user)
-    visit root_url
-    click_link("Host Trivia Game!")
-    fill_in("Title", with: "Test Game")
-    fill_in("Pin", with: "1234")
-    fill_in("Question time", with: 10)
-    click_button("Create Game")
-    click_link("Start Game")
-    click_link("Pause Game")
-    expect(page).to have_link("Start Game")
+  VCR.use_cassette('creates_a_new_message') do
+    it "logs in and creates a game" do
+      login(user)
+      visit root_url
+      click_link("Host Trivia Game!")
+      fill_in("Title", with: "Test Game")
+      fill_in("Pin", with: "1234")
+      fill_in("Question time", with: 10)
+      click_button("Create Game")
+      expect(page).to have_content("Test Game")
+      click_link("Start Game")
+      click_link("Pause Game")
+      expect(page).to have_link("Start Game")
+    end
   end
 
   it "does not let you create game while not logged in" do
