364c7f2e534594b2241f0728e918d5638884c44b
Christopher <ChrisKnight.mail@gmail.com>
2013-06-29 12:33:41 -0600

    fixed last spec

--- spec/features/un_authenticated_users/answers_question_correct_with_bad_spelling_spec.rb
+++ spec/features/un_authenticated_users/answers_question_correct_with_bad_spelling_spec.rb
@@ -2,14 +2,21 @@ require 'spec_helper'
 
 describe "as a public visitor" do 
   context "when I type a response to a question" do 
-   let(:game){FactoryGirl.create(:game)}
-   let(:question){FactoryGirl.create(:question)}
+   
+    before(:each) do 
+      Game.delete_all
+      Question.delete_all
+      @game = FactoryGirl.create(:game)
+      @question = FactoryGirl.create(:question, :game_id => @game.id)
+      visit '/'
+    end
 
     it "answers a question correctly but spelled incorrectly" do
       visit '/'
       fill_in "team_name", with: "Power Rangers"
       fill_in "game_pin", with: "MyString"
       click_link_or_button "Join Game"
+      # save_and_open_page
       fill_in "submission[content]", with: "oceun"
       click_link_or_button "Submit Answer"
       expect(page).to have_content "spelled"
