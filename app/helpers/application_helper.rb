cb8da37387f46db8506f48d89c462c5f3792bb78
Josh <joshua.mejia@gmail.com>
2013-07-03 12:13:43 +0000

    Adjust settings and layouts for production server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def broadcast(channel, &block)
     message = { :channel => channel, :data => capture(&block) }
-    uri = URI.parse("http://localhost:9292/faye")
+    uri = URI.parse("http://brewsandqs.com:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 
